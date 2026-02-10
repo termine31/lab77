697c70069e2e638aebf1ef148472f3cec58056e2
termine1 <gsafdq.tn.qe12@gmail.com>
2026-02-10 21:48:55 +0700

    Доделал задание с кредитами,вводится сумма, срок и ставка также добавил объяснения к переменным чтоб понимать какая переменная за что отвечает в кредите Оформил по заданию, также выводится в конце сколько всего процентов накапало за всё время

--- lab77/lab77.cpp
+++ lab77/lab77.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main() {
     // Задание 2 
     int mx;
+    printf("2 задание\n");
     printf("Введите максимальное значение MAX\n");
     scanf("%d", &mx);
 
@@ -28,7 +29,7 @@ int main() {
         printf("F(%d) = %d Сумма первых чисел фибоначчи %d \n", count,temp,sum);
 
         if (sum > mx) {
-            printf("Сумма превысила MAX на числе %d\n", count);
+            printf("Сумма превысила MAX на числе %d\n\n", count);
             break;
         }
         count++;
@@ -36,9 +37,34 @@ int main() {
     }
 
     // Задание 3 
-    int S, srok, procent;
+    int N;
+    double S,R; // S - сумма кредита N - срок R - ставка
+    printf("-----------------------------------------------------------------------------------------------------------------------\n");
+    printf("3 Задание\n"); 
     printf("Введите сумму, срок и процентную ставку кредита: ");
-    scanf("%d %d &d", &S,&srok,&procent);
+    scanf("%lf %d %lf", &S,&N,&R);
+    printf("-----------------------------------\n");
+    printf("    Долг    Процент    Платеж\n");
+    printf("-----------------------------------\n");
+
+    double  main_payment; // основной платеж 
+    main_payment = S / N; // платеж делится на весь срок равномерно 
+    double monthly_rate; 
+    monthly_rate = R / 100 / 12; // ставка за месяц
+    double total_interest = 0;
+
+
+    for (int i = 1; i <= N; i++) {
+        double debt, interest, payment;
+        debt = S - (i - 1) * main_payment; // остаток долга на следующий месяц, в 1 месяц он такой, какой был изначально 
+        interest = debt * monthly_rate; // процент за тек.месяц
+        payment = main_payment + interest; // складываю процент за месяц + основной платеж за месяц 
+        total_interest += interest; // складываю проценты 
+        printf("%3d %9.2lf %9.2lf %9.2lf\n", i, debt, interest, payment);
+    }
+    printf("-----------------------------------\n");
+    printf("Всего процентов: %.2lf \n", total_interest);
+
 
     return 0;
 }
